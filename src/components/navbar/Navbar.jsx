3fddb808a1f0fac4ed5b7cad43cfff58fef88714
aliseyedi <solafiali657@gmail.com>
2023-03-14 14:39:03 +0330

    add prettier plugin tailwindcss

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,24 +2,24 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="bg-zinc-400 h-16 flex flex-row justify-between items-center container px-20 ">
-      <h2 className=" text-2xl font-bold font-mono cursor-pointer">Recipe App</h2>
+    <div className=" container flex h-16 flex-row items-center justify-between bg-zinc-400 px-20 ">
+      <h2 className=" cursor-pointer font-mono text-2xl font-bold">Recipe App</h2>
 
-      <ul className="flex flex-row gap-8 justify-start items-center ">
-        <li className="text-1xl font-bold font-serif">
+      <ul className="flex flex-row items-center justify-start gap-8 ">
+        <li className="text-1xl font-serif font-bold">
           <Link to="/">Home</Link>
         </li>
-        <li className="text-1xl font-bold font-serif">
+        <li className="text-1xl font-serif font-bold">
           <Link to="/categories">Category</Link>
         </li>
-        <li className="text-1xl font-bold font-serif">
+        <li className="text-1xl font-serif font-bold">
           <Link to="/about">About</Link>
         </li>
       </ul>
 
       <div className="flex gap-3">
-        <button className="border rounded-xl bg-red-400 px-5 py-2">Log In</button>
-        <button className="border rounded-xl bg-red-400 px-4 py-2">Sign Up</button>
+        <button className="rounded-xl border bg-red-400 px-5 py-2">Log In</button>
+        <button className="rounded-xl border bg-red-400 px-4 py-2">Sign Up</button>
       </div>
     </div>
   );
