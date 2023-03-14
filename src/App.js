b027943a3237e7b50982b2c982b9a97b21dfad20
aliseyedi <solafiali657@gmail.com>
2023-03-14 13:36:25 +0330

    add categories page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./../src/Pages/home/Home";
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/about/About";
 import NotFound from "./Pages/notFound/NotFound";
+import Categories from "./Pages/categories/Categories";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/404" element={<NotFound />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/categories/Categories.jsx
@@ -0,0 +1,5 @@
+function Categories() {
+  return <h2>categories</h2>;
+}
+
+export default Categories;
