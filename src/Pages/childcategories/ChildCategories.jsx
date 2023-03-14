13a835a86516b2b9e7099511197abd25ac848e9b
aliseyedi <solafiali657@gmail.com>
2023-03-15 00:13:06 +0330

    update child categories page

--- src/Pages/childcategories/ChildCategories.jsx
+++ src/Pages/childcategories/ChildCategories.jsx
@@ -22,17 +22,16 @@ function ChildCategories() {
   const catName = useParams();
 
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-full w-full flex-col  backdrop-blur-2xl">
       <Navbar />
 
-      <h1 className="text-center text-4xl font-bold uppercase text-yellow-400">Choose Your Recipe</h1>
-      <div className=" justify-center1 mt-2 flex flex-wrap items-center gap-2 px-2 ">
+      <div className=" scrollbar-hide mt-1 flex flex-wrap items-center justify-center gap-2 overflow-y-scroll scroll-auto px-2 ">
         {images.map((recipe) => {
           const { strMeal, strMealThumb } = recipe;
           return (
             <div className="mb-5 flex h-36  w-36 flex-col items-center justify-between ">
               <Link to={`/recipe/${strMeal}`}>
-                <div className=" flex h-full  w-full flex-col items-center justify-between  rounded-2xl p-5 text-white backdrop-blur-2xl duration-300 hover:scale-105 hover:cursor-pointer">
+                <div className=" flex h-full  w-full flex-col items-center justify-between  rounded-2xl p-5 text-white  duration-300 hover:scale-105 hover:cursor-pointer">
                   <img className="h-full  w-full rounded-lg text-center" src={strMealThumb} alt={strMeal} />
                   <h2 className="text-md w-20 truncate  text-start font-bold">{strMeal}</h2>
                 </div>
