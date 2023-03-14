e6eb672e860cb44a303d663eaa5e9db3b0ad235b
aliseyedi <solafiali657@gmail.com>
2023-03-14 19:51:27 +0330

    update categories page

--- src/Pages/categories/Categories.jsx
+++ src/Pages/categories/Categories.jsx
@@ -1,9 +1,44 @@
 import Navbar from "../../components/navbar/Navbar";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Categories() {
+  const [image, setImages] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+      .then((data) => {
+        const image = data.data.categories;
+        setImages(image);
+        console.log(image);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
+
+      <div className="flex flex-wrap items-center justify-center  ">
+        {image.map((recipe) => {
+          const { strCategory, strCategoryThumb } = recipe;
+
+          return (
+            <div className="flex h-44 w-48  flex-col place-content-center">
+              <Link to={"/category/" + strCategory}>
+                <div className="category-info m-1  rounded-2xl p-5 text-white backdrop-blur-2xl duration-300 hover:scale-105 hover:cursor-pointer">
+                  <h2 className=" text-center text-2xl font-bold">{strCategory}</h2>
+                  <img className="h-full w-full" src={strCategoryThumb} alt={strCategory} />
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
