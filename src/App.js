3ffa465b93486f9408e01e8e99b10e4b8e31b9bf
aliseyedi <solafiali657@gmail.com>
2023-03-14 13:15:47 +0330

    add about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./../src/Pages/home/Home";
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/about/About";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
