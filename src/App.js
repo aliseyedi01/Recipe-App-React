32eee29284f1c4c21790a42973010182a8928825
aliseyedi <solafiali657@gmail.com>
2023-03-14 12:57:53 +0330

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./../src/Pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+    <div>
+      <Home />
     </div>
   );
 }
