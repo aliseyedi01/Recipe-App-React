31dc54f6b9f93962cd937dd1a26d68f174433b2a
aliseyedi <solafiali657@gmail.com>
2023-03-14 17:07:42 +0330

    fix config border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    borderRadius: {
-      image: "40% 60% 27% 73% / 48% 55% 45% 52% ",
+    extend: {
+      borderRadius: {
+        image: "40% 60% 27% 73% / 48% 55% 45% 52% ",
+      },
     },
-    extend: {},
   },
   plugins: [require("prettier-plugin-tailwindcss")],
 };
